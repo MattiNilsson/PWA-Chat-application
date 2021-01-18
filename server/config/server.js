b7d5786493eb2a2ccfb42e7c821b23fe7569240e
unknown <matti.nilsson980105@gmail.com>
2021-01-18 15:30:16 +0100

    Start of Project

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15fd36ad16639b803d5e2bfa57789807'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2ef6f2bf-dbe5-48f9-a3ac-4238222fd0fe'
+};
